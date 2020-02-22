9a5d91649428ef893c3e899b009ee7ebba48c7b4
jaeyson <contact@jaeyson.com>
2020-02-22 21:39:18 +0700

    wip basic algo

--- lib/freecodecamp/basic_algo.ex
+++ lib/freecodecamp/basic_algo.ex
@@ -13,5 +13,10 @@ defmodule Freecodecamp.BasicAlgo do
 
   """
   @spec convert_to_f(integer) :: integer
-  def convert_to_f(celsius) when is_integer(celsius), do: celsius * 9 / 5 + 32
+  def convert_to_f(celsius \\ 0) when is_integer(celsius), do: celsius * 9 / 5 + 32
+
+  def reverse_string(str) when is_binary(str), do: String.reverse str
+
+  def factorialize(0), do: 1
+  def factorialize(n) when is_integer(n), do: n * factorialize(n - 1)
 end
