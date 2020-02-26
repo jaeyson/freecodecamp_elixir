882947b408d70efabec9d3d7836497c95fe037f4
jaeyson <hello@jaeyson.dev>
2020-02-26 08:57:33 +0700

    find longest word length

--- lib/freecodecamp/basic_algo.ex
+++ lib/freecodecamp/basic_algo.ex
@@ -19,4 +19,12 @@ defmodule Freecodecamp.BasicAlgo do
 
   def factorialize(0), do: 1
   def factorialize(n) when is_integer(n), do: n * factorialize(n - 1)
+
+  def find_longest_wordlength(""), do: 0
+  def find_longest_wordlength(str) when is_binary(str) do
+    str
+    |> String.split()
+    |> Enum.max_by(&String.length &1)
+    |> String.length()
+  end
 end
