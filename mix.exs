e93600cf56f253e0a4ccd951634acfc7f8965e8d
jaeyson <hello@jaeyson.dev>
2022-01-19 14:54:06 +0800

    update ci.yml

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Freecodecamp.MixProject do
     [
       app: :freecodecamp,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
       aliases: aliases(),
